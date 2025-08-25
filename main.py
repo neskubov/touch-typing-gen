43869a56882aa02711daba165c74da6ffc8bd6d9
neskubov <51290924+neskubov@users.noreply.github.com>
2025-08-25 18:17:30 +0300

    исправлена ошибка с подсчетом символов

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import zipfile
 num_of_characters = 10000 #Задаем количество символов
 
 def gen_sequence(chars, length=1000):
-    result = []
-    for i in range(length):
-        for num_space in range(random.randint(3, 11)):
-            c = random.choice(chars)
-            result.append(c)
-        result.append(" ")
-    return "".join(result)
+    result = ""
+    counter_symbols = 0
+    while counter_symbols < length:
+        length_word = random.randint(3, 11)
+        for _ in range(length_word):
+            symbol = random.choice(chars)
+            result += symbol
+            counter_symbols += 1
+            if counter_symbols >= length:
+                break
+        result += " "
+        counter_symbols += 1
+    return result
 
 tasks = [
     ("ваол", "seq1.txt"),
@@ -27,7 +33,6 @@ tasks = [
 # создаём текстовые файлы
 for chars, fname in tasks:
     seq = gen_sequence(chars, num_of_characters)
-    print(seq)
     with open(fname, "w", encoding="utf-8") as f:
         f.write(seq)
 
