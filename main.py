aafdfc838424a178156d1c2613c75d887b7ed1f4
neskubov <neskubov@yandex.ru>
2025-09-19 08:58:25 +0300

    Fixed: characters no longer repeat

--- main.py
+++ main.py
@@ -1,67 +1,123 @@
 import random
 import zipfile
+import os
+# import psutil
+# process = psutil.Process(os.getpid())
+# print(f"Использование памяти: {process.memory_info().rss / 1024 ** 2:.2f} MB")
 
 num_of_characters = 5000 #Задаем количество символов
 
 def gen_sequence_level_one(chars, length=1000):
     result = ""
     counter_symbols = 0
-    max_weight = int(length // len(chars))
-    grade_weight = {char:0 for char in chars}
+    choice_chars = chars
     while counter_symbols < length:
-        symbol = random.choice(chars)
+        symbol = random.choice(choice_chars)
         result += symbol + " "
-        symbol_weight = grade_weight[symbol] + 1
-        grade_weight[symbol] = symbol_weight
-        if symbol_weight >= max_weight:
-            chars = chars.replace(symbol, "")
+
+        choice_chars = choice_chars.replace(symbol, "")
+
+        if not choice_chars:
+            choice_chars = chars
+
         counter_symbols += 1
+
     return result
 
 
 def gen_sequence_level_two(chars, length=1000):
-    result = ""
+    result = chars
     counter_symbols = 0
     while counter_symbols < length:
         length_word = random.randint(3, 11)
         for _ in range(length_word):
             symbol = random.choice(chars)
+
+            if result[-1] == symbol:
+                continue
+
             result += symbol
             counter_symbols += 1
+
             if counter_symbols >= length:
                 break
+
         result += " "
         counter_symbols += 1
+
     return result
 
 
-'''tasks = [
-    ("ваол", "seq1_level_one.txt", "seq1_level_two.txt"),
-    ("ыдфж", "seq2_level_one.txt", "seq2_level_two.txt"),
-    ("прен", "seq3_level_one.txt", "seq3_level_two.txt"),
-    ("мьит", "seq4_level_one.txt", "seq4_level_two.txt"),
-    ("кгуш", "seq5_level_one.txt", "seq5_level_two.txt"),
-    ("цщйз", "seq6_level_one.txt", "seq6_level_two.txt"),
-    ("сбчю", "seq7_level_one.txt", "seq7_level_two.txt"),
-    ("я.э\\", "seq8_level_one.txt", "seq8_level_two.txt"),
-    ("ёх-ъ", "seq9_level_one.txt", "seq9_level_two.txt")
-]'''
-
-tasks = [("фываолдж", "seq1_level_one.txt", "seq1_level_two.txt")]
+tasks = [
+    "ваол",
+    "ыдфж",
+    "ваолыдфж",
+    "прен",
+    "ыдфжпрен"
+    "ваолыдфжпрен",
+    "мьит",
+    "пренмьит",
+    "ыдфжпренмьит",
+    "ваолыдфжпренмьит",
+    "кгуш",
+    "мьиткгуш",
+    "пренмьиткгуш",
+    "ыдфжпренмьиткгуш",
+    "ваолыдфжпренмьиткгуш",
+    "цщйз",
+    "кгушцщйз",
+    "мьиткгушцщйз",
+    "пренмьиткгушцщйз",
+    "ыдфжпренмьиткгушцщйз",
+    "ваолыдфжпренмьиткгушцщйз",
+    "сбчю",
+    "цщйзсбчю",
+    "кгушцщйзсбчю",
+    "мьиткгушцщйзсбчю",
+    "сбчюпренмьиткгушцщйз",
+    "сбчюыдфжпренмьиткгушцщйз",
+    "сбчюваолыдфжпренмьиткгушцщйз",
+    "я.э\\",
+    "я.э\\сбчю",
+    "я.э\\цщйзсбчю",
+    "я.э\\кгушцщйзсбчю",
+    "я.э\\мьиткгушцщйзсбчю",
+    "я.э\\сбчюпренмьиткгушцщйз",
+    "я.э\\сбчюыдфжпренмьиткгушцщйз",
+    "я.э\\сбчюваолыдфжпренмьиткгушцщйз",
+    "ёх-ъ",
+    "ёх-ъя.э\\",
+    "ёх-ъя.э\\сбчю",
+    "ёх-ъя.э\\цщйзсбчю",
+    "ёх-ъя.э\\кгушцщйзсбчю",
+    "ёх-ъя.э\\мьиткгушцщйзсбчю",
+    "ёх-ъя.э\\сбчюпренмьиткгушцщйз",
+    "ёх-ъя.э\\сбчюыдфжпренмьиткгушцщйз",
+    "ёх-ъя.э\\сбчюваолыдфжпренмьиткгушцщйз"
+         ]
 
 # создаём текстовые файлы
-for chars, fname_level_one, fname_level_two  in tasks:
-    seq_level_one = gen_sequence_level_one(chars)
-    seq_level_two = gen_sequence_level_two(chars, num_of_characters)
-    with open(fname_level_one, "w", encoding="utf-8") as f:
-        f.write(seq_level_one)
-    with open(fname_level_two, "w", encoding="utf-8") as f:
-        f.write(seq_level_two)
-
-# упаковываем в архив
+number_file = 1
 with zipfile.ZipFile("sequences.zip", "w") as zf:
-    for _, fname_1, fname_2  in tasks:
-        zf.write(fname_1)
-        zf.write(fname_2)
+    for chars in tasks:
+        fname_level_one = f'seq_{number_file}_level_one.txt'
+        fname_level_two = f'seq_{number_file}_level_two.txt'
+
+        seq_level_one = gen_sequence_level_one(chars, num_of_characters)
+        seq_level_two = gen_sequence_level_two(chars, num_of_characters)
+
+        with open(fname_level_one, "w", encoding="utf-8") as f:
+            f.write(seq_level_one)
+
+        with open(fname_level_two, "w", encoding="utf-8") as f:
+            f.write(seq_level_two)
+
+        zf.write(fname_level_one)
+        zf.write(fname_level_two)
+
+        os.remove(fname_level_one)
+        os.remove(fname_level_two)
+
+        number_file += 1
 
 print("Готово! Файл sequences.zip создан.")
